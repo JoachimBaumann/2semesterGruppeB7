6abc4c660447198f7766c246e025f8500456c811
Joachim <jobau19@student.sdu.dk>
2021-05-01 18:52:12 +0200

    removed setUID() because you dont change a Unique ID

--- src/main/java/Domain/Catalog/Person.java
+++ src/main/java/Domain/Catalog/Person.java
@@ -41,10 +41,6 @@ public class Person {
         return uID;
     }
 
-    public void setuID(int uID) {
-        this.uID = uID;
-    }
-
     @Override
     public String toString() {
         return "Person{" +
