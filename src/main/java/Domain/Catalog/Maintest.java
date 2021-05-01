dc96dde0d0661a5df424a4ec6da1b097753b9c32
Joachim <jobau19@student.sdu.dk>
2021-05-01 16:10:49 +0200

    Minor fixes

--- src/main/java/Domain/Catalog/Maintest.java
+++ src/main/java/Domain/Catalog/Maintest.java
@@ -6,11 +6,13 @@ public class Maintest {
     public static void main(String[] args) {
         Catalog catalog = new Catalog();
 
-        catalog.createProduction(1,new Date(01052021),"Bat-Jogge returns");
-        catalog.getProduction(1).addToProduction(new Credit("Batman",new Person("Joachim","Baumann",42606930,1)));
-        catalog.getProduction(1).addToProduction(new Credit("Robin",new Person("Phillip","Vincent",12345678,2)));
+        catalog.createProduction(1,new Date(01052021),"Batman vs Superman");
+        catalog.getProduction(1).addToProduction(new Credit("Superman",new Person("Joachim","Baumann",42606930,1)));
+        catalog.getProduction(1).addToProduction(new Credit("The Snoozer",new Person("Phillip","Vincent",12345678,2)));
 
         System.out.println(catalog.getProduction(1));
+        System.out.println(catalog.getProduction(1).getProductionName());
+        System.out.println(catalog.getProduction(1).getCreditList().get(2));
 
     }
 
