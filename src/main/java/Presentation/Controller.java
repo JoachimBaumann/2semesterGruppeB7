34fc6553cc82bcad302315390deba6bf5a87e13f
phillipvincent <phnie19@student.sdu.dk>
2021-05-01 18:56:57 +0200

    Added lightmode =)

--- src/main/java/Presentation/Controller.java
+++ src/main/java/Presentation/Controller.java
@@ -2,15 +2,27 @@ package Presentation;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class Controller {
     public HBox hBoxSignIn;
+    public VBox vBoxSignIn;
+    public Button bDark;
+    public ImageView darkBackground;
 
     public void bSignIn(ActionEvent actionEvent) {
-        hBoxSignIn.setVisible(true);
+        vBoxSignIn.setVisible(true);
     }
     public void bCancel(ActionEvent actionEvent){
-        hBoxSignIn.setVisible(false);
+        vBoxSignIn.setVisible(false);
+    }
+    public void bDark(ActionEvent actionEvent){
+            if (darkBackground.isVisible() == true){
+                darkBackground.setVisible(false);
+            }else{
+                darkBackground.setVisible(true);
+            }
     }
 }
