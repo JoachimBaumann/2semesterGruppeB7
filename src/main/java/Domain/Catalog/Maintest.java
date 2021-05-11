9fc31bfdc0d60b676410114e5b86956cbba21434
Joachim <jobau19@student.sdu.dk>
2021-05-11 13:40:32 +0200

    Implemented a Testclass for Persistance test

--- src/main/java/Domain/Catalog/Maintest.java
+++ src/main/java/Domain/Catalog/Maintest.java
@@ -1,18 +1,17 @@
 package Domain.Catalog;
 
+import Persistens.PersistanceHandler;
+
 import java.util.Date;
 
 public class Maintest {
     public static void main(String[] args) {
         Catalog catalog = new Catalog();
 
-        catalog.createProduction(1,new Date(21,05,01),"Batman vs Superman");
-        catalog.getProduction(1).addToProduction(new Credit("Superman",new Person("Joachim","Baumann",42606930,1)));
-        catalog.getProduction(1).addToProduction(new Credit("Lex Luther",new Person("Phillip","Vincent",12345678,2)));
 
-        System.out.println(catalog.getProduction(1));
-        System.out.println(catalog.getProduction(1).getProductionName());
-        System.out.println(catalog.getProduction(1).getCreditList().get(2));
+        PersistanceHandler p = PersistanceHandler.getInstance();
+
+        System.out.println(p.getPerson(1));
 
     }
 
