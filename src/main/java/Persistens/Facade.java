ca1e7c2bdefad4d0ee3ad2c0ec8d5e541cbfd565
Nadia <nadianiemier@hotmail.com>
2021-05-11 12:01:43 +0200

    Facade er ændret

--- src/main/java/Persistens/Facade.java
+++ src/main/java/Persistens/Facade.java
@@ -1,5 +1,76 @@
 package Persistens;
 
-public class Facade {
+import Domain.Catalog.Production;
+import Domain.CreditManager;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class Facade implements CreditManager {
+    private static Facade instance;
+    private String url = "localhost";
+    private int port = 5432;
+    private String databaseName = "Projekt";
+    private String username = "postgres";
+    private String password = "skriv din kode";
+    private Connection connection = null;
+
+    private Facade(){
+        initializePostgresqlDatabase();
+    }
+
+    public static Facade getInstance(){
+        if (instance == null) {
+            instance = new Facade();
+        }
+        return instance;
+    }
+
+    private void initializePostgresqlDatabase() {
+        try {
+            DriverManager.registerDriver(new org.postgresql.Driver());
+            connection = DriverManager.getConnection("jdbc:postgresql://" + url + ":" + port + "/" + databaseName, username, password);
+        } catch (SQLException | IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        } finally {
+            if (connection == null) System.exit(-1);
+        }
+    }
+
+    @Override
+    public void ViewAllProductions() {
+
+    }
+
+    @Override
+    public Production chooseProduction(int productionID) {
+        return null;
+    }
+
+    @Override
+    public void addCredit(String fName, String lName, String jobTitle, int phoneNumber) {
+
+    }
+
+    @Override
+    public void confirmChanges() {
+
+    }
+
+    @Override
+    public void findProduction() {
+
+    }
+
+    @Override
+    public void generateReport(int productionID, Date releaseDate) {
+
+    }
+
+    @Override
+    public void saveReport() {
+
+    }
 }
