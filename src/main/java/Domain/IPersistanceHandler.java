3cd53eddf23e3c12ee258722590d4bf6785593bc
Joachim <jobau19@student.sdu.dk>
2021-05-11 12:15:23 +0200

    Added methods for IPersistanceHandler.java

--- src/main/java/Domain/IPersistanceHandler.java
+++ src/main/java/Domain/IPersistanceHandler.java
@@ -2,7 +2,29 @@ package Domain;
 
 public interface IPersistanceHandler {
 
+    //Production
     public void getProductions();
+    public void getProduction(int productionID);
+    public boolean setProduction();
+    public void searchProduction(String name);
+    public void updateProduction();
+    public void deleteProduction(int productionID);
+    public void addProduction();
+
+    //Credit
+    public void getCredits();
+    public void getCredit(int creditID);
+    public void updateCredit();
+    public void deleteCredit(int creditID);
+    public void addCredit();
+
+
+    //Person
+    public void getPersons();
+    public void getPerson(int personID);
+    public void updatePerson();
+    public void deletePerson(int PersonID);
+    public void addPerson();
 
 
 
