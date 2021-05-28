04be1bc3456b5036baa3ad3ea551f5c6e2464974
Joachim <jobau19@student.sdu.dk>
2021-05-28 10:21:23 +0200

    CatalogTest

---
+++ src/test/java/Domain/Catalog/CatalogTest.java
@@ -0,0 +1,34 @@
+package Domain.Catalog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CatalogTest {
+
+    @Test
+    void getProductionList() {
+
+    }
+
+    @Test
+    void createProduction() {
+    }
+
+    @Test
+    void addProductionToCatalog() {
+        Production production = new Production(1,"01.01.2000","Test Produktion","Test");
+    }
+
+    @Test
+    void getPersonsList() {
+    }
+
+    @Test
+    void getProduction() {
+    }
+
+    @Test
+    void addCreditToProduction() {
+    }
+}
