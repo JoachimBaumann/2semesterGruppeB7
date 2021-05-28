f34a5e849aa3f8f9fc6a305fa090c0645d6245e6
phillipvincent <phnie19@student.sdu.dk>
2021-05-28 13:05:16 +0200

    Added testing of methods

--- src/test/java/Domain/Catalog/CatalogTest.java
+++ src/test/java/Domain/Catalog/CatalogTest.java
@@ -1,14 +1,27 @@
 package Domain.Catalog;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CatalogTest {
+    Catalog catalog = new Catalog();
+    Production production = new Production(1,"20-05-1999","Testfilm","Filmen tester shit");
+    ArrayList<Production> productions = new ArrayList<Production>();
+    void makeList(){
+        for (int i = 0; i < 20; i++) {
+            Production production = new Production(i,String.valueOf(i),String.valueOf(i),String.valueOf(i));
+            productions.add(production);
+        }
+    }
 
     @Test
     void getProductionList() {
-
+        makeList();
+        assertEquals(20, productions.size());
     }
 
     @Test
@@ -16,12 +29,14 @@ class CatalogTest {
     }
 
     @Test
-    void addProductionToCatalog() {
-        Production production = new Production(1,"01.01.2000","Test Produktion","Test");
+    void testaddProductionToCatalog() {
+    catalog.addProductionToCatalog(this.production.getProductionID(),this.production);
+    assertNotNull(catalog.getProduction(1));
     }
 
     @Test
     void getPersonsList() {
+
     }
 
     @Test
